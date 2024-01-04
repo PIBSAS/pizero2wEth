b870fc2f2110a75cd036ddc37fa31b111c70fd81
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2024-01-03 21:27:02 -0300

    Update pi02WEth.sh

--- pi02WEth.sh
+++ pi02WEth.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+sed -i '$a\dtoverlay=dwc2' /boot/config.txt
+sed -i 's/$/ modules-load=dwc2/' /boot/cmdline.txt
+sed -i '$a\libcomposite' /etc/modules
+wget -c "https://github.com/PIBSAS/pizero2wEth/main/usb-gadget.sh" -P "/usr/local/sbin/"
