c4b11df3290e57425e0bfc3d5cc4b6e7ff705ee7
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2024-01-03 22:25:57 -0300

    Update pi02WEth.sh

--- pi02WEth.sh
+++ pi02WEth.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+cd
 sed -i '$a\dtoverlay=dwc2' /boot/config.txt
+echo
 sed -i 's/$/ modules-load=dwc2/' /boot/cmdline.txt
+echo
 sed -i '$a\libcomposite' /etc/modules
+echo
 sudo wget -c "https://github.com/PIBSAS/pizero2wEth/blob/main/usb-gadget.sh" -P "/usr/local/sbin/"
 echo
 sudo chmod +x /usr/local/sbin/usb-gadget.sh
@@ -11,17 +15,18 @@ echo
 sudo systemctl enable usbgadget.service
 echo
 echo "Network Manager Settings"
+echo
 sudo nmcli con add type bridge ifname br0
 echo
 sudo nmcli con add type bridge-slave ifname usb0 master br0
 echo
 sudo nmcli con add type bridge-slave ifname usb1 master br0
 echo
-sudo nmcli connection modify bridge-br0 ipv4.method manual ipv4.addresses 10.55.0..1/24
+sudo nmcli connection modify bridge-br0 ipv4.method manual ipv4.addresses 10.55.0.1/24
 echo
 echo "dnsMasq"
 echo
-sudo apt intall -y dnsmasq
+sudo apt install -y dnsmasq
 echo
-sudo wget -c "https://github.com/PIBSAS/pizero2wEth/blob/main/br0" -P "/etc/dnsmaasq.d/"
+sudo wget -c "https://github.com/PIBSAS/pizero2wEth/blob/main/br0" -P "/etc/dnsmasq.d/"
 echo
